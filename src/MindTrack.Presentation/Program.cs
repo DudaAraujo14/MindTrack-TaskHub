c0bc27f9edde3fef4a1189d5a1cac400060bf376
DudaAraujo14 <rm560944@fiap.com.br>
2025-11-01 14:36:39 -0300

    feat(api): configurar API principal com injeção de dependência, EF Core e endpoints iniciais

--- src/MindTrack.Presentation/Program.cs
+++ src/MindTrack.Presentation/Program.cs
@@ -1,41 +1,54 @@
+using MindTrack.Infrastructure;
+using MindTrack.Application.Mappings;
+using MindTrack.Presentation.Middlewares;
+using AutoMapper; 
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+var conn = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddInfrastructure(conn);
+
+// 🔄 AutoMapper
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+builder.Services.AddControllers();
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.InvalidModelStateResponseFactory = context =>
+    {
+        var erros = context.ModelState
+            .Where(e => e.Value.Errors.Count > 0)
+            .Select(e => new
+            {
+                Campo = e.Key,
+                Mensagens = e.Value.Errors.Select(x => x.ErrorMessage)
+            });
+
+        var resposta = new
+        {
+            status = 400,
+            erro = "BadRequest",
+            mensagem = "Um ou mais campos estão inválidos.",
+            detalhes = erros
+        };
+
+        return new BadRequestObjectResult(resposta);
+    };
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.UseMiddleware<ErrorHandlingMiddleware>();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
